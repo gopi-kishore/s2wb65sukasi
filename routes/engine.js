28f9761775de7e29271f8f6f1aae711bcb985ecf
gopi-kishore <sukasigopikishore1999@gmail.com>
2022-04-06 21:27:41 -0500

    updated file

--- routes/engine.js
+++ routes/engine.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+   res.render('engine', {title: 'Search results for engine'});
 });
 
 module.exports = router;
