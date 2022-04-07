cd9c3ddbf282fa48039d10a49b3ead397ddb71ce
gopi-kishore <sukasigopikishore1999@gmail.com>
2022-04-06 21:15:10 -0500

    added engine

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var turboengineRouter = require('./routes/turboengine');
+var engineRouter = require('./routes/engine');
 
 var app = express();
 
@@ -22,7 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/turboengine', turboengineRouter);
+app.use('/engine', engineRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
